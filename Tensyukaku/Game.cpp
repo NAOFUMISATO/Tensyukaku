9042c035ae69d145832871d6ea24eea232c0e92d
NAOFUMISATO <0117hyottoko@gmail.com>
2021-08-02 01:20:35 +0900

    プロジェクト ファイルを追加します。

---
+++ Tensyukaku/Game.cpp
@@ -0,0 +1,60 @@
+
+#include "DxLib.h"
+#include "Game.h"
+#include "ModeTitle.h"
+using namespace Tsk;
+// アプリの初期化
+// 起動時に1回だけ実行される
+Game::Game()
+{
+	_gKey = 0;
+	_gTrg = 0;
+	_gCnt = 0;
+	// モードサーバの初期化
+	_serverMode = new ModeServer(*this);
+	// タイトルモードを生成し、モードサーバに登録する
+	ModeTitle* modeTitle = new ModeTitle();
+	_serverMode->Add(modeTitle, 0, "Title");
+}
+
+// アプリの解放
+// 終了時に1回だけ実行される
+Game::~Game()
+{
+	delete _serverMode;
+}
+
+
+
+//
+// フレーム処理。1フレームに1回ずつ実行される
+//
+
+// フレーム処理：入力
+void Game::Input()
+{
+	int keyold = _gKey;
+	_gKey = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+	_gTrg = (_gKey ^ keyold) & _gKey;	// キーのトリガ情報生成（押した瞬間しか反応しないキー情報）
+}
+
+// フレーム処理：計算
+void Game::Process()
+{
+	_serverMode->ProcessInit();
+	_serverMode->Process();
+	_serverMode->ProcessFinish();
+
+	/*_mapChips.Process(*this);*/
+
+	_gCnt++;
+}
+
+// フレーム処理：描画
+void Game::Draw()
+{
+	_serverMode->DrawInit();
+	_serverMode->Draw();
+	_serverMode->DrawFinish();
+}
+
---
+++ Tensyukaku/Main.cpp
@@ -0,0 +1,47 @@
+#include "DxLib.h"
+#include <stdio.h>
+#include <time.h>
+#include "game.h"
+#include "ResourceServer.h"
+
+using namespace Tsk;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine, int nCmdShow)
+{
+	// 初期化 -------------------------------------------------------------------------
+	// ＤＸライブラリ初期化処理
+	SetGraphMode(SCREEN_W, SCREEN_H, SCREEN_DEPTH);
+	ChangeWindowMode(true);							// ウィンドウモードに指定する
+	if (DxLib_Init() == -1)
+	{	// エラーが起きたら直ちに終了
+		return -1;
+	}
+	SetDrawScreen(DX_SCREEN_BACK);		// 描画先画面を裏画面にセット
+
+	// 乱数初期化
+	srand((unsigned int)time(NULL));
+
+	// リソースサーバ初期化
+	ResourceServer::Init();
+
+	// ゲームクラス
+	Game* g = new Game();
+
+	// 1フレームループを組む ----------------------------------------------------------
+	while (ProcessMessage() == 0)		// プログラムが終了するまでループ
+	{
+		g->Input();		// 入力
+		g->Process();	// 計算
+		g->Draw();		// 描画
+	}
+
+	// 解放 ---------------------------------------------------------------------------
+	ResourceServer::Release();	// リソースサーバ解放
+
+	delete g;			// ゲームクラスの解放
+
+	DxLib_End();		// ＤＸライブラリ使用の終了処理
+
+	// 終了
+	return 0;
+}
